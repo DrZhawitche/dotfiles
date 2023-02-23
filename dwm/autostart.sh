f60a0c891394dd2f9bbb9e97161c0ddf120eb437
Dr_Zhawitche <103216281+DrZhawitche@users.noreply.github.com>
2023-02-23 20:59:39 +0000

    Delete dwm directory

--- dwm/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom -b &
-/usr/bin/emacs --daemon &
-nitrogen --restore &
-volumeicon &
-dwmblocks &
--- dwm/dmw-back/autostart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-picom -b &
-volumeicon &
-/usr/bin/emacs --daemon &
-feh --bg-scale ~/.config/dwm/wallpapers/forest-fog.jpg
